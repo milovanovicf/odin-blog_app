b63176af5594339f66e3f0f9a0dc0e61a2af25ca
Filip <milovanovicfilip007@gmail.com>
2022-02-23 17:27:26 +0100

    fixing code

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
-  
-  
   resources :articles do
     resources :comments
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
